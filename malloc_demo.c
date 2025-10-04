4715f2b923b5f31773b9d12ce3d2e8190f07fc3e
Pratham1801 <116380431+Pratham1801@users.noreply.github.com>
2025-10-04 13:18:49 +0530

    Add files via upload

---
+++ malloc_demo.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int lenght = 0;
+    int *Arr = NULL;
+
+    printf("Enter the number of elements : \n");
+    scanf("%d",&lenght);
+
+    //Step 1 : Allocate the Memory
+    Arr = (int *)malloc(lenght* sizeof(int));
+    if(Arr == NULL)
+    {
+        printf("Unable to allocate memory \n");
+    }
+    else 
+    {
+        printf("Memory gets succesfully allocated \n");
+    }
+
+    //Step 2 : Use the Memory
+
+    //Step 3: Free the Memory
+    free(Arr);
+
+    return 0;
+}
